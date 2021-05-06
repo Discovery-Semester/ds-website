317ca7262f352e1c3e3fa07b73a7f27e619fbb16
Simon Frei <freisim93@gmail.com>
2021-05-06 10:35:35 +0200

    Rename to Au_x to evade windows reserved filename

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -12,7 +12,7 @@ import constants from "../common/constants";
 import Button from "@material-ui/core/Button";
 import {toggleSideDrawer} from "../store/actions/uiActionCreator";
 import {Link, NavLink} from "react-router-dom";
-import Aux from "../hoc/Aux";
+import Aux from "../hoc/Au_x";
 
 const useStyles = makeStyles({
     list: {
