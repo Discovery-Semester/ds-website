c9a86e91ba23187372d3567251603bc094afaa82
Simon Frei <freisim93@gmail.com>
2021-07-11 23:05:21 +0200

    Add about quotes and pics

--- src/assets/about/team/quotes.tsx
+++ src/assets/about/team/quotes.tsx
@@ -1,13 +1,25 @@
 // The name has to be exactly the same as the file name !
 export default {
     "en": {
-    "Antoine": "Chemist by training but considering becoming a chef so I can at leasteat my products",
-    "Robert": "Chemist by training but considering becoming a chef so I can at leasteat my products",
-    "Samuel": "Chemist by training but considering becoming a chef so I can at leasteat my products",
+        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products",
+        "Annina": "Always look ahead - but don’t forget what’s going on left and right as well.",
+        "Elena": "I hate being bored, so I set myself the goal of making the world a better place.",
+        "Florian": "I always set my goals way too high, like becoming world champion in something I only discovered a year ago.",
+        "Jasmin": "I always keep an eye on organization - unless I’m distracted by chocolate.",
+        "Karolina": "I like to try new things - so I sometimes wash my hair with flour.",
+        "Matthias": "Be the change you want to see in the world.",
+        "Paola": "I believe that life is either a daring adventure..or nothing at all!",
+        "Simon": "Always sticking to my principles (well trying), and always looking for alternative views to change mine.",
     },
     "de": {
-        "Antoine": "Chemist",
-        "Robert": "Chemist by training but considering becoming a chef so I can at leasteat my products",
-        "Samuel": "Chemist by training but considering becoming a chef so I can at leasteat my products",
+        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products",
+        "Annina": "Always look ahead - but don’t forget what’s going on left and right as well.",
+        "Elena": "I hate being bored, so I set myself the goal of making the world a better place.",
+        "Florian": "I always set my goals way too high, like becoming world champion in something I only discovered a year ago.",
+        "Jasmin": "I always keep an eye on organization - unless I’m distracted by chocolate.",
+        "Karolina": "I like to try new things - so I sometimes wash my hair with flour.",
+        "Matthias": "Be the change you want to see in the world.",
+        "Paola": "I believe that life is either a daring adventure..or nothing at all!",
+        "Simon": "Bin immer meinen Prinzipien treu (versuchs wenigstens), und suche immer nach anderen Ansichten um meine zu ändern.",
     },
 }
