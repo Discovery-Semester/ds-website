cf73f3836bedacce5d39faf0a3d55b4584de9f0b
Simon Frei <freisim93@gmail.com>
2021-08-06 21:22:59 +0200

    Add German team quotes.

--- src/assets/about/team/quotes.tsx
+++ src/assets/about/team/quotes.tsx
@@ -1,7 +1,7 @@
 // The name has to be exactly the same as the file name !
 export default {
     "en": {
-        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products",
+        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products.",
         "Annina": "Always look ahead - but don’t forget what’s going on left and right as well.",
         "Elena": "I hate being bored, so I set myself the goal of making the world a better place.",
         "Florian": "I always set my goals way too high, like becoming world champion in something I only discovered a year ago.",
@@ -12,14 +12,14 @@ export default {
         "Simon": "Always sticking to my principles (well trying), and always looking for alternative views to change mine.",
     },
     "de": {
-        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products",
-        "Annina": "Always look ahead - but don’t forget what’s going on left and right as well.",
-        "Elena": "I hate being bored, so I set myself the goal of making the world a better place.",
-        "Florian": "I always set my goals way too high, like becoming world champion in something I only discovered a year ago.",
-        "Jasmin": "I always keep an eye on organization - unless I’m distracted by chocolate.",
-        "Karolina": "I like to try new things - so I sometimes wash my hair with flour.",
+        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products.",
+        "Annina": "Schau immer nach Vorne - aber vergiss nicht, was links und rechts von dir los ist.",
+        "Elena": "Ich kann Langeweile nicht leiden, also habe ich mir das Ziel gesetzt, die Welt zu einem besseren Ort zu machen.",
+        "Florian": "Ich setze meine Ziele immer viel zu hoch an, wie Weltmeister in etwas zu werden, das ich erst vor einem Jahr entdeckt habe.",
+        "Jasmin": "Ich behalte die Organisation immer im Auge - es sei denn, ich werde durch Schokolade abgelenkt.",
+        "Karolina": "Ich probiere gerne neue Dinge aus -  deswegen wasche ich meine Haare manchmal mit Mehl.",
         "Matthias": "Be the change you want to see in the world.",
-        "Paola": "I believe that life is either a daring adventure..or nothing at all!",
+        "Paola": "Ich glaube, dass das Leben entweder ein gewagtes Abenteuer ist... oder gar nichts!",
         "Simon": "Bin immer meinen Prinzipien treu (versuchs wenigstens), und suche immer nach anderen Ansichten um meine zu ändern.",
     },
 }
