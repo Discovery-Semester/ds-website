f9af2be5951563553ef8619e900c4760d830ade2
Simon Frei <freisim93@gmail.com>
2022-04-25 23:36:32 +0200

    New quote for Amrita

--- src/assets/about/team/quotes.tsx
+++ src/assets/about/team/quotes.tsx
@@ -1,12 +1,12 @@
 // The name has to be exactly the same as the file name !
 export default {
     "en": {
-        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products.",
+        "Amrita": "Always promoting strength in diversity, especially when it comes to pineapple on pizza.",
         "Idil": "It's not lonely at the top if you help others get there, too.",
         "Simon": "Always sticking to my principles (well trying), and always looking for alternative views to change mine.",
     },
     "de": {
-        "Amrita": "Chemist by training but considering becoming a chef so I can at least eat my products.",
+        "Amrita": "Always promoting strength in diversity, especially when it comes to pineapple on pizza.",
         "Idil": "It's not lonely at the top if you help others get there, too.",
         "Simon": "Bin immer meinen Prinzipien treu (versuchs wenigstens), und suche immer nach anderen Ansichten um meine zu ändern.",
     },
